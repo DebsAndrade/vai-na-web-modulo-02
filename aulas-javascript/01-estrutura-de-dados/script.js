929e474e729c8af14a01d778877e52d7fdfc6e54
deboraandrade <deboraellenandrade@gmail.com>
2022-06-09 13:13:01 +0100

    first commit

---
+++ aulas-javascript/01-estrutura-de-dados/script.js
@@ -0,0 +1,25 @@
+// Crie uma variável que receba um nome;
+let nome = "Débora Andrade"
+console.log(nome)
+
+// Crie uma variável que receba um número;
+let numero = 27
+console.log(numero)
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let ano = 2022
+console.log(ano)
+console.log(ano - numero)
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+console.log(ano + numero)
+
+// Mostre no console a mensagem "Estou mexendo no console.log";
+console.log("Estou mexendo no console.log.")
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = false
+console.log(quartaFeira)
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof quartaFeira)
