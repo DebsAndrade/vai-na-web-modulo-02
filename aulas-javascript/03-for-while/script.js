8dc56fae2c737a9d2594f2b0c0e93f4910d01565
deboraandrade <deboraellenandrade@gmail.com>
2022-06-14 12:19:30 +0100

    Pratique for e while, tarefa 03

---
+++ aulas-javascript/03-for-while/script.js
@@ -0,0 +1,32 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+console.log("Criar loop que conte de 1 até 10 usando FOR.")
+for (let count = 1; count <= 10; count += 1){
+    console.log(count)
+} 
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+console.log("Criar loop que conte de 10 até 1 usando WHILE.")
+let conte = 10
+
+while (conte >= 1) {
+    console.log(conte)
+    conte -= 1
+}
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+console.log("Criar loop que conte os números ímpares de 1 até 100.")
+let contador = 1
+
+while(100 >=  contador){
+    console.log(contador);
+    contador +=2
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+console.log("Criar loop que conte os números pares de 0 até 100.")
+let contar = 0
+
+while(100 >=  contar){
+    console.log(contar);
+    contar +=2
+}
