61a4c133147dbb211962057426186f16acff6291
deboraandrade <deboraellenandrade@gmail.com>
2022-06-26 13:23:06 +0100

    Adicione + um desafio do VnW, estrutura de Dados

---
+++ aulas-javascript/05-estrutura-de-dados-complexos/script.js
@@ -0,0 +1,19 @@
+ // Crie um array que receba 5 itens e exiba no console.
+let nome = ["Débora", "Ellen", "Oliveira", "de", "Andrade"]
+console.log(nome)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+nome.unshift("Olá")
+
+// Utilize um método para remover o último nome do array.
+nome.pop()
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+nome.push("Moro em", "Portugal")
+
+// Utilize um método para remover o primeiro nome do array.
+nome.shift()
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+let numbers = [7,5,6,3,8,9,2,1,4]
+console.log(numbers.sort())
